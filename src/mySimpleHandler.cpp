e00ad6425144e9c626bbee5eb75585c4c317198f
Виталий Суслов <arthas.menethil2002@bk.ru>
2022-03-04 16:44:14 +0700

    Make mySimpleHandler lib

---
+++ src/mySimpleHandler.cpp
@@ -0,0 +1,5 @@
+//
+// Created by vitaly on 27.02.2022.
+//
+
+#include "mySimpleHandler.h"
---
+++ src/mySimpleHandler.h
@@ -0,0 +1,14 @@
+//
+// Created by vitaly on 27.02.2022.
+//
+
+#ifndef ARCHITECT_MYSIMPLEHANDLER_H
+#define ARCHITECT_MYSIMPLEHANDLER_H
+
+
+class mySimpleHandler {
+
+};
+
+
+#endif //ARCHITECT_MYSIMPLEHANDLER_H
